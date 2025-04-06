315b7208942123a9858a53ea3fa074686a8b39fd
Marc Ramiro <marcramiro@gmail.com>
2025-04-06 15:05:32 +0200

    fix filter

--- Library/Features/GetBooksList/V1/Query.cs
+++ Library/Features/GetBooksList/V1/Query.cs
@@ -1,4 +1,6 @@
+using System.Text.RegularExpressions;
 using Library.Entities;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Library.Features.GetBooksList.V1;
@@ -9,10 +11,12 @@ public static class Query
     {
         var filterBuilder = Builders<Book>.Filter;
         
+        var regexOptions = new RegexOptions[] { RegexOptions.IgnoreCase }; 
+
         var filterDefinition = filterBuilder.Or(
-            filterBuilder.Regex(b => b.Title, filter),
-            filterBuilder.Regex(b => b.Authors, filter)
-            );
+            filterBuilder.Regex(b => b.Title, new BsonRegularExpression(filter, string.Join("", regexOptions.Select(r => r.ToString().Substring(0,1).ToLower())))),
+            filterBuilder.Regex(b => b.Authors, new BsonRegularExpression(filter, string.Join("", regexOptions.Select(r => r.ToString().Substring(0, 1).ToLower()))))
+        );
         return !string.IsNullOrWhiteSpace(filter) 
             ? filterBuilder.And(filterDefinition,filterBuilder.Eq(q=> q.Status,Status.Active) ) 
             : filterBuilder.Eq(q=> q.Status,Status.Active);
