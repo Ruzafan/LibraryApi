d0b819273bed34a7f6a37735dce2cbf11e3f3db2
Ruzafan <marcramiro@gmail.com>
2025-04-30 20:35:38 +0200

    fix

--- Library/Features/GetUserBookList/V1/Route.cs
+++ Library/Features/GetUserBookList/V1/Route.cs
@@ -7,7 +7,7 @@ public static class Route
 {
     public static void MapUserBookListEndpoint(this WebApplication app)
     {
-        app.MapGet("/library/userbook/v1/", async ([FromQuery] bool wishlist, [FromQuery] string friendId, HttpContext httpContext, CancellationToken cancellationToken, Handler handler) =>
+        app.MapGet("/library/userbook/v1/", async ([FromQuery] bool wishlist, [FromQuery] string? friendId, HttpContext httpContext, CancellationToken cancellationToken, Handler handler) =>
             {
                 var userId = httpContext.User.Claims.First(q=> q.Type == ClaimTypes.Name).Value;
                 var request = new Request() { UserId = string.IsNullOrEmpty(friendId) ? userId : friendId, WishList=wishlist };
